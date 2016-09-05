d6cd6ebec63bbeaef9c737c40b09736f79ceaa3a
Marcin Tolysz <tolysz@gmail.com>
2016-09-05 14:44:31 +0100

    lts-6.15

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 stack --stack-yaml stack-base.yaml setup
+stack --stack-yaml stack-base.yaml install alex happy hscolour
+
 PATH=`stack path --bin-path  --stack-yaml stack-base.yaml 2>/dev/null`:$PATH
 echo SETUP
 date
@@ -11,11 +13,11 @@ date
 stack build
 date
 PP=`stack path --local-install-root`/bin/reflex-todomvc.jsexe
-servius -d $PP -p 3301 &
-WW=$!
+#servius -d $PP -p 3301 &
+#WW=$!
 
 stack build --file-watch
 #echo CLOSURE-COMPILE
 #ccjs $PP/all.js --compilation_level=ADVANCED_OPTIMIZATIONS --externs=node > ../server/static/v2.js
 
-kill $WW
+#kill $WW
